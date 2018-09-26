299b8eb945763a39600f8e317d403c821281be37
Yogesh Joshi <yjoshi@yodlee.com>
2018-09-26 18:12:39 +0530

    JadooVsKoba is solution for practice problem of question available in hacker-earth

---
+++ Python_Practice/JadooVsKoba.py
@@ -0,0 +1,18 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+letters = ['F','G','H','I','J','K','L','M','N','O','P']
+for l in letters:
+    n = ord(l)
+    print(n)
+    
+
+    
+
